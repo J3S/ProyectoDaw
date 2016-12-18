92f0376e3c3f5c5b2c4c42d1aaf796c3f91d2e0c
J3S <johnnysuarez@outlook.com>
2016-12-17 19:44:00 -0500

    Termina página de personal de la materia

--- js/personal.js
+++ js/personal.js
@@ -10,6 +10,11 @@ function llenarAyudantes() {
         agregarContenedor(data.ayudantesClases, 'header-ayudante-clase', ayudantesC, i);
         agregarContenedor(data.ayudantesDeberes, 'header-ayudante-deber', ayudantesD, i);
      });
+     $.getJSON("../../data/personal/profesores.json", function(data) {
+        var i = 0;
+        agregarContenedor2(data.coordinador, 'header-coordinador', coordinador, i);
+        agregarContenedor2(data.profesores, 'header-profesor', profesores, i);
+     });
 }
 
 function agregarContenedor(datos, strId, elemento_agregar, index) {
@@ -24,6 +29,32 @@ function agregarContenedor(datos, strId, elemento_agregar, index) {
             var correo = $('<label>' + value.correo + '</label>');
             content.append(nombre);
             content.append(correo);
+            avatar.append(img);
+            card.append(canvas);
+            card.append(avatar);
+            card.append(content);
+            contenedor.append(card);
+            index++;
+            elemento_agregar.append(contenedor);
+    });
+}
+
+function agregarContenedor2(datos, strId, elemento_agregar, index) {
+    $.each(datos, function(key, value) {
+            var contenedor = $('<div class="col-sm-4 contenedor-card"></div>');
+            var card = $('<div class="card"></div>');
+            var canvas = $('<canvas class="header-bg" width="250" height="70" id="' + strId + index + '"></canvas>');
+            var avatar = $('<div class="avatar"></div>');
+            var img = $('<img src="' + value.rutaImagen + '" alt="" />');
+            var content = $('<div class="content"></div>');
+            var nombre = $('<label>' + value.nombre + '</label><br>');
+            var correo = $('<label>' + value.correo + '</label><br>');
+            var oficina = $('<label>Oficina: ' + value.oficina + '</label><br>');
+            var horario = $('<label>Horario: ' + value.horario + '</label>');
+            content.append(nombre);
+            content.append(correo);
+            content.append(oficina);
+            content.append(horario);
             avatar.append(img);
             card.append(canvas);
             card.append(avatar);
